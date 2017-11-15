1e4d88db37b5da2624c5f3e2ce5ad88a0f2e3a78
yujk745 <yujk745@gmail.com>
2017-11-15 15:57:57 +0900

    새 파일

---
+++ myproject/2.base/namespace/namespace.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+namespace funcNum1{
+    int a(int x){
+        return x*x;
+    }
+}
+
+namespace funcNum2{
+    int a(int x){
+        return x-(x*x);
+    }
+}
+
+int main(void){
+    //std::cout<<a(5)<<'\n'; //¿¡·¯!
+    std::cout<<funcNum1::a(5)<<'\n'; //25
+    std::cout<<funcNum2::a(5)<<'\n'; //-20
+    return 0;
+}
