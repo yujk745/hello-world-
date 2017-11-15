5818fdccc37241d055d5ad86b8939570a90b5a56
yujk745 <yujk745@gmail.com>
2017-11-15 16:35:42 +0900

    이름 변경(cin.cc->cinInt.cc)

---
+++ myproject/2.base/keyInput/cinInt.cc
@@ -0,0 +1,12 @@
+/*
+    이름: cin.cc
+    설명: cin을 이용하여 정수값 키보드 입력을 받아 출력하는 코드.
+*/
+#include <iostream>
+
+int main(void){
+    int yourAge=0;
+    std::cout<<"나이를 입력하세요: ";
+    std::cin>>yourAge; //입력받음, enter 입력시 종료.
+    std::cout<<"나이: "<<yourAge<<"살"<<'\n';
+}
