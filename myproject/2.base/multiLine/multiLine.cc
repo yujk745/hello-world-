cca23c549b8ac20147eca2baa0649f3437de6653
yujk745 <yujk745@gmail.com>
2017-11-15 15:38:34 +0900

    프로그램 설명, 주석 추가

--- myproject/2.base/multiLine/multiLine.cc
+++ myproject/2.base/multiLine/multiLine.cc
@@ -1,6 +1,11 @@
+/*
+    이름: multiLine.cc
+    설명: 문장의 출력시 줄바꿈을 실험함.
+*/
 #include <iostream>
 
 int main(void){
+    //'\n'과 "\n"이 모두 유효한지 확인하기 위해 모두 사용해봄.
     std::cout<<"Hello"<<'\n'<<"World!"<<"\n"<<"end."<<std::endl;
     return 0;
 }
