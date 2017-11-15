fd4ec0958b5137422743aad02769a6cb38e4ecc6
yujk745 <yujk745@gmail.com>
2017-11-15 16:40:17 +0900

    주석 추가

--- myproject/2.base/keyInput/cinChar.cc
+++ myproject/2.base/keyInput/cinChar.cc
@@ -1,10 +1,14 @@
+/*
+    이름: cinChar.cc
+    설명: cin을 이용하여 Char형 키보드 입력을 받아 출력하는 코드.
+*/
 #include <iostream>
 
 int main(void){
-    char name[16];
-    char* yourName = name;
+    char name[16]; //입력값이 저장될 배열
+    char* yourName = name; //name 배열을 가리키는 포인터
 
     std::cout<<"이름을 입력하세요: ";
-    std::cin>>yourName;
+    std::cin>>yourName; //입력받음
     std::cout<<"이름: "<<yourName<<'\n';
 }
